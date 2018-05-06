c9e09a875887c76cc2fb81d5968eaaef4dce36ba
dviveiros <daniel.viveiros@gmail.com>
2018-05-05 18:17:51 -0700

    Unit test for FifaRankDAO

--- src/main/java/com/ciandt/paul/dao/FifaRankDAO.java
+++ src/main/java/com/ciandt/paul/dao/FifaRankDAO.java
@@ -3,14 +3,21 @@ package com.ciandt.paul.dao;
 import com.ciandt.paul.Config;
 import com.ciandt.paul.entity.FifaRank;
 import com.ciandt.paul.utils.BigQueryUtils;
+import com.ciandt.paul.utils.GCSUtils;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Class responsible for reading Fifa Rank data for a specific year
@@ -24,27 +31,60 @@ public class FifaRankDAO {
     private Config config;
     @Autowired
     private BigQueryUtils bigQueryUtils;
+    @Autowired
+    private GCSUtils gcsUtils;
+
+    private static Map<Integer, List<FifaRank>> cache;
+
+    static {
+        cache = new HashMap();
+    }
 
     /**
      * Read the rank for a specific year
      */
     public List<FifaRank> fetch(Integer year) throws IOException, InterruptedException {
-        List<FifaRank> fifaRankList = new ArrayList<>();
-        List<List<String>> queryResult = null;
 
-        String query = "SELECT * FROM paul_the_octopus_dataset.fifa_rank LIMIT 300";
-        queryResult = bigQueryUtils.executeQuery(query);
-        FifaRank fifaRank = null;
-
-        for (List<String> row : queryResult) {
-            fifaRank = new FifaRank();
-            fifaRank.setRank(Integer.valueOf(row.get(0)));
-            fifaRank.setTeamCode(row.get(1));
-            fifaRank.setTeamName(row.get(2));
-            fifaRank.setPoints(Integer.parseInt(row.get(3)));
-            fifaRankList.add(fifaRank);
+        //check the cache
+        if (cache.get(year) != null) {
+            return cache.get(year);
         }
 
+        List<FifaRank> fifaRankList = new ArrayList<>();
+        FifaRank fifaRank = null;
+
+        // The data for 2006, 2010 and 2014 is inside GCS, bucket project-paul-the-octopus-datasets
+        // 2018 is on BigQuery
+        if ((year == 2006) || (year == 2010) || (year == 2014)) {
+            String filename = "fifa_rank_" + year + ".csv";
+            String content = gcsUtils.readFile(config.getDatasetBucket(), filename);
+            Reader in = new StringReader(content);
+            Iterable<CSVRecord> records = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(in);
+            for (CSVRecord record : records) {
+                fifaRank = new FifaRank();
+                fifaRank.setRank(Integer.valueOf(record.get(0)));
+                fifaRank.setTeamCode(record.get(1));
+                fifaRank.setTeamName(record.get(2));
+                fifaRank.setPoints(Integer.parseInt(record.get(3)));
+                fifaRankList.add(fifaRank);
+            }
+        } else {
+            List<List<String>> queryResult = null;
+
+            String query = "SELECT * FROM paul_the_octopus_dataset.fifa_rank LIMIT 300";
+            queryResult = bigQueryUtils.executeQuery(query);
+
+            for (List<String> row : queryResult) {
+                fifaRank = new FifaRank();
+                fifaRank.setRank(Integer.valueOf(row.get(0)));
+                fifaRank.setTeamCode(row.get(1));
+                fifaRank.setTeamName(row.get(2));
+                fifaRank.setPoints(Integer.parseInt(row.get(3)));
+                fifaRankList.add(fifaRank);
+            }
+        }
+
+        cache.put(year, fifaRankList);
         return fifaRankList;
     }
 }
--- src/test/java/com/ciandt/paul/dao/FifaRankDAOTest.java
+++ src/test/java/com/ciandt/paul/dao/FifaRankDAOTest.java
@@ -3,6 +3,7 @@ package com.ciandt.paul.dao;
 import com.ciandt.paul.Config;
 import com.ciandt.paul.entity.FifaRank;
 import com.ciandt.paul.utils.BigQueryUtils;
+import com.ciandt.paul.utils.GCSUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +20,7 @@ import static org.junit.Assert.assertNotNull;
  * Test FifaRank data access object
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {FifaRankDAO.class, Config.class, BigQueryUtils.class})
+@SpringBootTest(classes = {FifaRankDAO.class, Config.class, BigQueryUtils.class, GCSUtils.class})
 public class FifaRankDAOTest {
 
     @Autowired
@@ -34,9 +35,54 @@ public class FifaRankDAOTest {
     }
 
     @Test
-    public void shouldFetchFifaRankPerYear() throws Exception {
+    public void shouldFetchFifaRankFor2018() throws Exception {
         List<FifaRank> fifaRankList = fifaRankDAO.fetch(2018);
         assertNotNull(fifaRankList);
         assertEquals(211, fifaRankList.size());
+    }
+
+    @Test
+    public void shouldFetchFifaRankFor2006() throws Exception {
+        List<FifaRank> fifaRankList = fifaRankDAO.fetch(2006);
+        assertNotNull(fifaRankList);
+        assertEquals(32, fifaRankList.size());
+
+        assertEquals("BRA", fifaRankList.get(0).getTeamCode());
+        assertEquals("Brazil", fifaRankList.get(0).getTeamName());
+        assertEquals(new Integer(827), fifaRankList.get(0).getPoints());
+
+        assertEquals("TOG", fifaRankList.get(31).getTeamCode());
+        assertEquals("Togo", fifaRankList.get(31).getTeamName());
+        assertEquals(new Integer(569), fifaRankList.get(31).getPoints());
+    }
+
+    @Test
+    public void shouldFetchFifaRankFor2010() throws Exception {
+        List<FifaRank> fifaRankList = fifaRankDAO.fetch(2010);
+        assertNotNull(fifaRankList);
+        assertEquals(32, fifaRankList.size());
+
+        assertEquals("BRA", fifaRankList.get(0).getTeamCode());
+        assertEquals("Brazil", fifaRankList.get(0).getTeamName());
+        assertEquals(new Integer(1611), fifaRankList.get(0).getPoints());
+
+        assertEquals("NKO", fifaRankList.get(31).getTeamCode());
+        assertEquals("North Korea", fifaRankList.get(31).getTeamName());
+        assertEquals(new Integer(292), fifaRankList.get(31).getPoints());
+    }
+
+    @Test
+    public void shouldFetchFifaRankFor2014() throws Exception {
+        List<FifaRank> fifaRankList = fifaRankDAO.fetch(2014);
+        assertNotNull(fifaRankList);
+        assertEquals(32, fifaRankList.size());
+
+        assertEquals("ESP", fifaRankList.get(0).getTeamCode());
+        assertEquals("Spain", fifaRankList.get(0).getTeamName());
+        assertEquals(new Integer(1485), fifaRankList.get(0).getPoints());
+
+        assertEquals("AUS", fifaRankList.get(31).getTeamCode());
+        assertEquals("Australia", fifaRankList.get(31).getTeamName());
+        assertEquals(new Integer(526), fifaRankList.get(31).getPoints());
     }
 }
